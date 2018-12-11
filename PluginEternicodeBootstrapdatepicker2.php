9f6ece5a6dc8cc3e166c6c51dcb76f9e14293453
Claes Östlund <claesostlund@Mefromhd-MBP18.local>
2018-12-11 10:56:15 +0100

    First version.

---
+++ PluginEternicodeBootstrapdatepicker2.php
@@ -0,0 +1,9 @@
+<?php
+class PluginEternicodeBootstrapdatepicker2{
+  public static function widget_include(){
+    $element = array();
+    $element[] = wfDocument::createHtmlElement('link', null, array('href' => '/plugin/eternicode/bootstrapdatepicker2/css/bootstrap-datepicker.min.css', 'rel' => 'stylesheet'));
+    $element[] = wfDocument::createHtmlElement('script', null, array('src' => '/plugin/eternicode/bootstrapdatepicker2/js/bootstrap-datepicker.min.js', 'type' => 'text/javascript'));
+    wfDocument::renderElement($element);
+  }
+}
