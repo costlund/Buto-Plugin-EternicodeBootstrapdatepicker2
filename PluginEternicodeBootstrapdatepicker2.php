21d11ded8508b03580d0caabd8c310b5ed4a3161
Claes Ö <claes@stenbergit.se>
2019-04-30 10:32:11 +0200

    Improvement.

--- PluginEternicodeBootstrapdatepicker2.php
+++ PluginEternicodeBootstrapdatepicker2.php
@@ -4,7 +4,8 @@ class PluginEternicodeBootstrapdatepicker2{
     $element = array();
     $element[] = wfDocument::createHtmlElement('link', null, array('href' => '/plugin/eternicode/bootstrapdatepicker2/css/bootstrap-datepicker.min.css', 'rel' => 'stylesheet'));
     $element[] = wfDocument::createHtmlElement('script', null, array('src' => '/plugin/eternicode/bootstrapdatepicker2/js/bootstrap-datepicker.min.js', 'type' => 'text/javascript'));
-    $element[] = wfDocument::createHtmlElement('script', null, array('src' => '/plugin/eternicode/bootstrapdatepicker2/PluginEternicodeBootstrapdatepicker2.js', 'type' => 'text/javascript'));
+    wfPlugin::enable('include/js');
+    $element[] = wfDocument::createWidget('include/js', 'include', array('src' => '/plugin/eternicode/bootstrapdatepicker2/PluginEternicodeBootstrapdatepicker2.js'));    
     wfDocument::renderElement($element);
   }
 }
